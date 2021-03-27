cc68ad04940db8828b7d84014523edb5ca34ded6
Testtsts <ihsannur0610@gmail.com>
2021-03-27 17:58:12 +0700

    soal1

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -10,27 +10,31 @@ grep -i -o -P '(?<=error).* ' `pwd`/syslog.log | sort | uniq -c | sort -nr |sed 
 
 echo
 echo "C. "
-arr=( $(grep -i -o -P '(?<=\().*(?=\))' `pwd`/syslog.log | sort | uniq) )
+arr=( $(grep -i -o -P '(?<=\().*(?=\))' `pwd`/syslog.log |sort|uniq) )
+
 for x in ${arr[@]} ; 
 do
-	errorc=$(grep -o -c "ERROR.*${x}" syslog.log)
-	infoc=$(grep -o -c "INFO.*${x}" syslog.log)
-	total=$((errorc + infoc))
-	echo "${x} Error = ${errorc} Info = ${infoc} Total = ${total}"
+	errorc=0
+	infoc=0
+	errorc=$(grep -o -c "ERROR.*${x})" syslog.log)
+	infoc=$(grep -o -c "INFO.*${x})" syslog.log)
+	#total=$((total+errorc+infoc))
+
+	echo "${x} Error = ${errorc}  Info = ${infoc}"
 done
 
 echo
-echo "D"
+#echo "D"
 
 echo Error,Count > `pwd`/error_message.csv
 grep -i -o -P '(?<=error).* ' `pwd`/syslog.log | sort | uniq -c | sort -nr|sed -E 's/^ *([0-9]||[0-9][0-9])  (.+)/\2,\1/' >> `pwd`/error_message.csv
 
 echo
-echo "E. "
+#echo "E. "
 echo Username,INFO,ERROR > `pwd`/user_statistics.csv
 for x in ${arr[@]} ; 
 do
-	errorc=$(grep -o -c "ERROR.*${x}" syslog.log)
-	infoc=$(grep -o -c "INFO.*${x}" syslog.log)
+	errorc=$(grep -o -c "ERROR.*${x})" syslog.log)
+	infoc=$(grep -o -c "INFO.*${x})" syslog.log)
 	echo "${x},${infoc},${errorc}" >> `pwd`/user_statistics.csv
 done
